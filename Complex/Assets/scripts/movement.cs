21d52b2d1426b07fea47f34bde022b473110d6b4
ZeroNu11 <levo950@gmail.com>
2020-03-22 16:09:18 +0300

    Add project

---
+++ Complex/Assets/scripts/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    float speed = 10f;
+    public Vector3 velocity;
+
+    public Transform groundCheck;
+    public LayerMask Mask;
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = transform.right * x + transform.forward * z + transform.up*0f;
+
+        velocity.y -= 9.81f;
+
+        if (Physics.CheckSphere(groundCheck.position, 0.5f, Mask))
+        {
+            velocity.y = -1f;
+        }
+
+        this.GetComponent<CharacterController>().Move(move * speed * Time.deltaTime);
+        this.GetComponent<CharacterController>().Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Complex/Assets/scripts/rotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float sensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation += mouseX;
+        transform.localRotation = Quaternion.Euler(xRotation,yRotation,0f);
+    }
+}
